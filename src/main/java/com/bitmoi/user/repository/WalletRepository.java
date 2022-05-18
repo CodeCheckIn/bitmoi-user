1f3dadc6fa929f3ebe84f779430f47ab3c0c0c19
Jungwook Kim <jw0905@naver.com>
2022-05-19 03:38:23 +0900

    test

--- src/main/java/com/bitmoi/user/repository/WalletRepository.java
+++ src/main/java/com/bitmoi/user/repository/WalletRepository.java
@@ -7,22 +7,21 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface WalletRepository extends ReactiveCrudRepository<Wallet, Long> {
     @Query("select "
-            + "(select quantity from WALLET where user_id=W.user_id and coin_id=10) krw,"
-            + "0 holdings,"
-            + "sum(W.avg_price*W.quantity) purchaseAmount,"
-            + "sum(W.quantity*C.price) appraisalAmount,"
-            + "0 valuationPL,"
-            + "0 yield"
-            + "from WALLET W,"
-            + "	 COIN C"
-            + "where W.user_id=:id"
-            + "and W.coin_id <>10"
+            + "(select quantity from WALLET where user_id=W.user_id and coin_id=10) krw, "
+            + "0 holdings, "
+            + "sum(W.avg_price*W.quantity) purchaseAmount, "
+            + "sum(W.quantity*C.price) appraisalAmount, "
+            + "0 valuationPL, "
+            + "0 yield "
+            + "from WALLET W, "
+            + "	 COIN C "
+            + "where W.user_id=:id "
+            + "and W.coin_id <>10 "
             + "group by user_id")
     Mono<WalletResponse> findByUserId(int id);
 
